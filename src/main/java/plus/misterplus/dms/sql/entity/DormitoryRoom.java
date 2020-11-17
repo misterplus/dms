a792ccaa3e9a40d13c2c23557a28162ce3f6c1c6
misterplus <whiteplayz45@gmail.com>
2020-11-17 20:42:46 +0800

    增加人数属性

--- src/main/java/plus/misterplus/dms/sql/entity/DormitoryRoom.java
+++ src/main/java/plus/misterplus/dms/sql/entity/DormitoryRoom.java
@@ -1,12 +1,13 @@
 package plus.misterplus.dms.sql.entity;
 
 public class DormitoryRoom {
-    private int dbno, drbd, drbno, dmno;
+    private int dbno, drbd, drbno, dmno, dcap;
 
-    public DormitoryRoom(int dbno, int drbd, int drbno) {
+    public DormitoryRoom(int dbno, int drbd, int drbno, int dcap) {
         this.dbno = dbno;
         this.drbd = drbd;
         this.drbno = drbno;
         this.dmno = -1;
+        this.dcap = dcap;
     }
 }
