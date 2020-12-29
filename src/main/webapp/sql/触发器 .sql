29b3aedd78de68d75e186637ce4454904fc1ee6d
chen2 <2397651454@qq.com>
2020-12-29 18:22:45 +0800

    功能完善+2

--- src/main/webapp/sql/触发器 .sql
+++ src/main/webapp/sql/触发器 .sql
@@ -12,22 +12,17 @@ create trigger fee_log on fees
 after update
 as
 begin
-	declare @oldfpaid bit ,@newfpaid bit
-	select 
-	@newfpaid=fpaid 
-	from 
-	inserted
-	select 
-	@oldfpaid=fpaid 
-	from 
-	deleted
+	declare @oldfpaid bit 
+	declare @newfpaid bit
+	select @newfpaid=fpaid 
+	from inserted
+	select @oldfpaid=fpaid 
+	from deleted
 	if @newfpaid=1
-		insert into
-		 log_history 
+		insert intolog_history 
 		 values('fees','未缴纳->未缴纳',getdate())
 	if @newfpaid=0
-		insert into 
-		log_history 
+		insert into log_history 
 		values('fees','已缴纳->未缴纳',getdate())
 	end
 go
@@ -37,18 +32,14 @@ create trigger item_in_log on items
 after insert
 as
 begin
-	declare @olditype bit ,@newitype bit
-	select 
-	@newitype=itype 
-	from 
-	inserted
-	select 
-	@olditype=itype 
-	from 
-	deleted
+	declare @olditype bit 
+	declare @newitype bit
+	select @newitype=itype 
+	from inserted
+	select @olditype=itype 
+	from deleted
 	if @newitype=0
-		insert into
-		log_history 
+		insert intolog_history 
 		values('items','空->存入',getdate())
 	end
 go
@@ -57,18 +48,14 @@ create trigger item_out_log on items
 after update
 as
 begin
-	declare @olditype bit ,@newitype bit
-	select 
-	@newitype=itype 
-	from 
-	inserted
-	select 
-	@olditype=itype 
-	from 
-	deleted
+	declare @olditype bit 
+	declare @newitype bit
+	select @newitype=itype 
+	from inserted
+	select @olditype=itype 
+	from deleted
 	if @newitype=1
-		insert into 
-		log_history 
+		insert into log_history 
 		values('items','存放->已取走',getdate())
 	end
 go
@@ -78,18 +65,14 @@ create trigger guest_in_log on guest
 after insert
 as
 begin
-	declare @oldgtype bit ,@newgtype bit
-	select 
-	@newgtype=gtype 
-	from 
-	inserted
-	select 
-	@oldgtype=gtype 
-	from 
-	deleted
+	declare @oldgtype bit 
+	declare @newgtype bit
+	select @newgtype=gtype 
+	from inserted
+	select @oldgtype=gtype 
+	from deleted
 	if @newgtype=0
-		insert into 
-		log_history 
+		insert into log_history 
 		values('guest','空->进入',getdate())
 	end
 go
@@ -98,18 +81,14 @@ create trigger guest_out_log on guest
 after update
 as
 begin
-	declare @oldgtype bit ,@newgtype bit
-	select 
-	@newgtype=gtype 
-	from 
-	inserted
-	select 
-	@oldgtype=gtype 
-	from 
-	deleted
+	declare @oldgtype bit
+	declare @newgtype bit
+	select @newgtype=gtype 
+	from inserted
+	select @oldgtype=gtype 
+	from deleted
 	if @newgtype=1
-		insert into 
-		log_history 
+		insert into log_history 
 		values('guest','进入->已离开',getdate())
 	end
 go
@@ -120,20 +99,13 @@ as
 begin 
 	declare @rsno char(8)
 	declare @restatus varchar(10)
-	select 
-	@restatus=restatus 
-	from 
-	inserted
-	select 
-	@rsno=rsno 
-	from 
-	inserted
-	update 
-	rsheet 
-	set 
-	rprogress=@restatus 
-	where 
-	rsno=@rsno
+	select @restatus=restatus 
+	from inserted
+	select @rsno=rsno 
+	from inserted
+	update rsheet 
+	set rprogress=@restatus 
+	where rsno=@rsno
 	end
 go
 
@@ -144,20 +116,13 @@ as
 begin 
 	declare @rsno char(8)
 	declare @restatus varchar(10)
-	select 
-	@restatus=restatus 
-	from 
-	inserted
-	select 
-	@rsno=rsno 
-	from 
-	inserted
-	update 
-	rsheet 
-	set 
-	rprogress=@restatus 
-	where 
-	rsno=@rsno
+	select @restatus=restatus 
+	from inserted
+	select @rsno=rsno 
+	from inserted
+	update rsheet 
+	set rprogress=@restatus 
+	where rsno=@rsno
 	end
 go
 
@@ -169,29 +134,39 @@ begin
 	declare @reno char(8)
 	declare @rsno char(8)
 	declare @restatus varchar(10)
-	select
-	@restatus=restatus 
+	select @restatus=restatus 
 	from inserted
-	select 
-	@rsno=rsno
-	from 
-	inserted
-	select 
-	@reno=reno 
-	from 
-	inserted
-	update 
-	rsheet 
-	set 
-	rprogress=@restatus 
-	where 
-	rsno=@rsno
-	update 
-	rsheet 
-	set 
-	reno=@reno 
-	where 
-	rsno=@rsno
+	select @rsno=rsno
+	from inserted
+	select @reno=reno 
+	from inserted
+	update rsheet 
+	set rprogress=@restatus 
+	where rsno=@rsno
+	update rsheet 
+	set reno=@reno 
+	where rsno=@rsno
 	end
 go
-
+--触发器6，增加寝室长
+alter trigger add_dmno on droom
+after update
+as
+begin
+	declare @drbno char(3)
+	declare @dbno char(2)
+	declare @dbd char(1)
+	declare @dmno_old char(8)
+	declare @dmno_new char(8)
+	select @drbno=drbno,@dbd=dbd,@dbno=dbno,@dmno_old=dmno
+	from deleted
+	select @dmno_new=dmno
+	from inserted
+	if @dmno_new not in (select sno
+							from student
+							where drbno=@drbno and dbd=@dbd and dbno=@dbno)
+		update droom 
+		set dmno=@dmno_old 
+		where drbno=@drbno and dbd=@dbd and dbno=@dbno and dmno=@dmno_new
+	end
+go
